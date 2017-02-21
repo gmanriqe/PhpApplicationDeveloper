98c37cf8d46ca4bf6750dcbc951dda271e396c38
Jesús Alexander Gonzales Manrique <gmanriqe@gmail.com>
2017-02-21 00:16:14 -0500

    Cambios v.3
    Haciendo uso de sesiones.

---
+++ sesion_03/Ejercicio02/test03.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: gmanriqe
+ * Date: 21/02/2017
+ * Time: 0:05
+ */
+    session_start();
+    $_SESSION['usuario'] = 'gmanriqe';
+    echo 'La variable de sesion fue creada';
---
+++ sesion_03/Ejercicio02/test04.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: gmanriqe
+ * Date: 21/02/2017
+ * Time: 0:09
+ */
+    session_start();
+    if( isset($_SESSION['usuario'])){
+        echo 'El valor de la variable usuario es: '.$_SESSION['usuario'];
+    } else{
+        echo 'La variable no existe';
+    }
