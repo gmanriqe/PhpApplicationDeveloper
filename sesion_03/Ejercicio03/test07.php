796dab4f545bc4bb215d4a7e081751b48c267c08
Jesús Alexander Gonzales Manrique <gmanriqe@gmail.com>
2017-02-21 16:41:42 -0500

    Cambios v.8
    Haciendo uso de la herencia (extends y parent::)

--- sesion_03/Ejercicio03/test07.php
+++ sesion_03/Ejercicio03/test07.php
@@ -29,9 +29,36 @@
         }
     }//fin de la clase Persona
 
+    Class Empleado extends Persona{
+        public $cargo;
+        public $sueldo;
+        public $fecha_ing;
+
+        public function __construct($dni,$nombre,$sexo,$fecha_nac,$cargo,$sueldo,$fecha_ing)
+        {
+            parent::__construct($dni,$nombre,$sexo,$fecha_nac);
+            $this->cargo = $cargo;
+            $this->sueldo = $sueldo;
+            $this->fecha_ing = $fecha_ing;
+        }
+
+    }//fin de la clase Empleado
+
     $persona = new Persona('71998055','Jesus Gonzales','M','1991-12-24');
+    $empleado = new Empleado('72558965','Mathieu Gonzales','M','2012-02-22','Jefe de Area',1500,'2017-02-21');
 
     echo $persona->getDni().'<br/>';
     echo $persona->nombre.'<br/>';
     echo $persona->sexo.'<br/>';
     echo $persona->fecha_nac.'<br/>';
+
+    echo '<br/>';
+
+    echo $empleado->getDni().'<br/>';
+    echo $empleado->nombre.'<br/>';
+    echo $empleado->sexo.'<br/>';
+    echo $empleado->fecha_nac.'<br/>';
+    echo $empleado->cargo.'<br/>';
+    echo $empleado->sueldo.'<br/>';
+    echo $empleado->fecha_ing.'<br/>';
+
