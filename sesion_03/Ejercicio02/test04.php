b913f4e8568bdbc19034a8581923b6ed84ad74c7
Jesús Alexander Gonzales Manrique <gmanriqe@gmail.com>
2017-02-21 00:22:47 -0500

    Cambios v.4
    Utilizando la funcion unset.

--- sesion_03/Ejercicio02/test04.php
+++ sesion_03/Ejercicio02/test04.php
@@ -8,6 +8,8 @@
     session_start();
     if( isset($_SESSION['usuario'])){
         echo 'El valor de la variable usuario es: '.$_SESSION['usuario'];
+        //con unset eliminamos cualquier tipo de variable
+        unset($_SESSION['usuario']);
     } else{
         echo 'La variable no existe';
     }
