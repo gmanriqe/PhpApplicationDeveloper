423751e1a136fa51993a902f662c245b23fb8acc
Jesús Alexander Gonzales Manrique <gmanriqe@gmail.com>
2017-02-21 16:31:53 -0500

    Cambios v.7
    Haciendo uso del metodo __destruct

---
+++ sesion_03/Ejercicio03/test07.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: gmanriqe
+ * Date: 21/02/2017
+ * Time: 16:25
+ */
+    Class Persona {
+        private $dni;
+        public $nombre;
+        public $sexo;
+        public $fecha_nac;
+        public function __construct($dni,$nombre,$sexo,$fecha_nac)
+        {
+            $this->dni=$dni;
+            $this->nombre=$nombre;
+            $this->sexo=$sexo;
+            $this->fecha_nac=$fecha_nac;
+        }
+
+        //solucion para la variable private $dni
+        public function getDni(){
+            return $this->dni;
+        }
+
+        public function __destruct()
+        {
+            echo 'Adios mundo cruel :(';
+        }
+    }//fin de la clase Persona
+
+    $persona = new Persona('71998055','Jesus Gonzales','M','1991-12-24');
+
+    echo $persona->getDni().'<br/>';
+    echo $persona->nombre.'<br/>';
+    echo $persona->sexo.'<br/>';
+    echo $persona->fecha_nac.'<br/>';
